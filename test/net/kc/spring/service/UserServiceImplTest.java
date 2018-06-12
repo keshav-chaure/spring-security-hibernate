b25155df7c50baee4860f267e0ccda4f20ded4e0
Keshav C Chaure <keshav.chaure@Yash.com>
2018-06-12 12:27:53 +0530

    updated with junit & mockito

---
+++ test/net/kc/spring/service/UserServiceImplTest.java
@@ -0,0 +1,53 @@
+package net.kc.spring.service;
+
+import net.kc.spring.dao.UserDao;
+import net.kc.spring.model.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import javax.persistence.ManyToOne;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by keshav.chaure on 6/12/2018.
+ */
+
+
+public class UserServiceImplTest {
+
+    private static final int USER_ID =1;
+
+    @Mock
+    private UserDao userDao;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findById() throws Exception {
+        User expectedUser=getUser();
+        Mockito.when(userService.findById(USER_ID)).thenReturn(expectedUser);
+
+        User user=userService.findById(USER_ID);
+
+        assertEquals("user id should be same ",expectedUser.getId(),user.getId());
+    }
+
+    private User getUser() {
+       User user=new User();
+       user.setId(USER_ID);
+       return  user;
+    }
+
+}
